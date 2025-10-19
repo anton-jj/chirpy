9ed421813c43ec6f88952b8ec17bd05397f0234a
anton <jappinen.anton@protonmail.com>
2025-10-19 14:40:30 +0200

    validating jwt token

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,17 +1,49 @@
 package auth
 
 import (
-"github.com/alexedwards/argon2id"
-"github.com/golang-jwt/golang-jwt"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
-fucn MakeJWT(userID, uuid.UUID, tokensecret string, expieresIn time.Duration) (string, error) {
-	
+func MakeJWT(userID uuid.UUID, tokenSecret string, expieresIn time.Duration) (string, error) {
+	currentTime := time.Now().UTC()
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(currentTime),
+		ExpiresAt: jwt.NewNumericDate(currentTime.Add(expieresIn)),
+		Subject:   userID.String(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrTokenSignatureInvalid
+		}
+		return tokenString, nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
+		return uuid.Parse(claims.Issuer), nil
+	}
+	return uuid.MustParse(token.Claims.GetIssuer())
 }
 
 func HashPassword(password string) (string, error) {
 
-	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams) 
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
 	if err != nil {
 		return "", err
 	}
